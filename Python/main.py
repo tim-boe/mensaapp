1d2017865683e76295595a9b468bfcc4b7ae0c05
tim3 <tim@boegel-online.de>
2023-06-02 19:05:14 +0200

    Added Rock Werchter Signed-off-by: tim3 <tim@boegel-online.de>

--- Python/main.py
+++ Python/main.py
@@ -9,6 +9,8 @@ academica_first = {}
 academica_bp = {}
 academica_wok = {}
 vita = {}
+rw_festival = {}
+rw_camping = {}
 time_limit = 1800
 
 
@@ -20,7 +22,10 @@ def start_page():
                  "af": len(academica_first),
                  "abp": len(academica_bp),
                  "aw": len(academica_wok),
-                 "vita": len(vita)}
+                 "vita": len(vita),
+                 "rwf": len(rw_festival),
+                 "rwc": len(rw_camping)}
+
     return render_template("index.html", customers=customers)
 
 
@@ -35,6 +40,19 @@ def academica_main_f():
         academica_main[name] = [x, y, submit_time]
         print(academica_main)
     return render_template("academica_main.html", locations=academica_main)
+
+
+@app.route("/rw_festival", methods=['POST', 'GET'])
+def rw_festival_f():
+    name = request.form.get("name")
+    x = request.form.get("x")
+    y = request.form.get("y")
+    submit_time = time.time()
+    if name is not None:
+        delete_old(rw_festival, limit=3600)
+        rw_festival[name] = [x, y, submit_time]
+        print(rw_festival)
+    return render_template("rw_festival.html", locations=rw_festival)
 
 
 @app.route("/academica_secret", methods=['POST', 'GET'])
@@ -62,12 +80,12 @@ def vita_f():
     return render_template("baustelle.html", locations=vita)
 
 
-def delete_old(locations):
+def delete_old(locations, limit=time_limit):
     current_time = time.time()
     valid_locations = {}
     for key, value in locations.items():
         if value is not None:
-            if current_time - value[2] < time_limit:
+            if current_time - value[2] < limit:
                 valid_locations[key] = value
 
     locations.clear()
